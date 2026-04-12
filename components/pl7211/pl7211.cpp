bb42b2b065a4c3be4f5cfbbffac172d77556e8dd
onemikedelta <22591818+onemikedelta@users.noreply.github.com>
2026-04-13 02:22:05 +0300

    changed some logic

--- components/pl7211/pl7211.cpp
+++ components/pl7211/pl7211.cpp
@@ -8,90 +8,130 @@ static const char *const TAG = "pl7211";
 
 void PL7211Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up PL7211...");
+  this->spi_setup();
   
-  this->spi_setup(); // Initialize ESPHome SPI bus
+  // PL7211'in kendi DSP'sinin uyanıp MTP hafızasını yüklemesi için kısa bir bekleme
+  delay(150); 
 
-  // 1. Take control of GPIO from DSP to CFG Register (0x380D = 0xF7)
-  this->write_register(0x380D, 0xF7);
+  // 1. GPIO kontrolünü DSP'den al, SPI CFG Register'ına ver (App Note Syf: 28)
+  this->write_register_8(0x380D, 0xF7);
 
-  // 2. Set GPIO 9, 12, 13 as Outputs in DIR Register (0x3903)
-  // GPIO9 = Bit 1, GPIO12 = Bit 4, GPIO13 = Bit 5. 
-  // (1<<1) | (1<<4) | (1<<5) = 0x32
-  this->write_register(0x3903, 0x32);
+  // 2. GPIO 9, 12, 13'ü Çıkış (Output) yap. (16-bit register adresi: 0x3902)
+  uint16_t dir_mask = (1 << 9) | (1 << 12) | (1 << 13);
+  this->write_register_16(0x3902, dir_mask);
 }
 
 void PL7211Component::dump_config() {
-  ESP_LOGCONFIG(TAG, "PL7211 Component:");
-  LOG_PIN("  CS Pin: ", this->cs_);
-  LOG_SENSOR("  ", "Voltage", this->voltage_sensor_);
-  LOG_SENSOR("  ", "Current", this->current_sensor_);
-  LOG_SENSOR("  ", "Power", this->power_sensor_);
+  ESP_LOGCONFIG(TAG, "PL7211 Component Setup Complete.");
 }
 
-void PL7211Component::write_register(uint16_t addr, uint8_t data) {
-  this->enable(); // Automatically pulls CS low
-  this->write_byte(0x41); // Command: Write, No CRC, 1 Byte
+void PL7211Component::write_register_8(uint16_t addr, uint8_t data) {
+  this->enable(); 
+  this->write_byte(0x41); // Komut 0x41: Yazma, CRC Yok, 1 Byte
   this->write_byte(addr >> 8);
   this->write_byte(addr & 0xFF);
   this->write_byte(data);
-  this->disable(); // Automatically pulls CS high
+  this->disable();
 }
 
-uint32_t PL7211Component::read_register_32(uint16_t addr) {
+void PL7211Component::write_register_16(uint16_t addr, uint16_t data) {
   this->enable();
-  this->write_byte(0x34); // Command: Read, No CRC, 4 Bytes
+  this->write_byte(0x42); // Komut 0x42: Yazma, CRC Yok, 2 Byte
   this->write_byte(addr >> 8);
   this->write_byte(addr & 0xFF);
-  
-  uint32_t val = 0;
-  val |= (uint32_t)this->read_byte() << 24;
-  val |= (uint32_t)this->read_byte() << 16;
-  val |= (uint32_t)this->read_byte() << 8;
-  val |= (uint32_t)this->read_byte();
-  
+  this->write_byte(data & 0xFF); // Low Byte ilk gider
+  this->write_byte(data >> 8);   // High Byte sonra gider
+  this->disable();
+}
+
+uint16_t PL7211Component::read_register_16(uint16_t addr) {
+  this->enable();
+  this->write_byte(0x32); // Komut 0x32: Okuma, CRC Yok, 2 Byte
+  this->write_byte(addr >> 8);
+  this->write_byte(addr & 0xFF);
+  uint16_t val = 0;
+  val |= this->read_byte();            // Low Byte
+  val |= (this->read_byte() << 8);     // High Byte
   this->disable();
   return val;
 }
 
-float PL7211Component::read_register_float(uint16_t addr) {
-  uint32_t raw_val = this->read_register_32(addr);
-  // PL7211 DSP converts internally to standard IEEE-754 32-bit Float
-  float float_val;
-  memcpy(&float_val, &raw_val, sizeof(float_val));
-  return float_val;
+uint64_t PL7211Component::read_register_48(uint16_t addr) {
+  this->enable();
+  this->write_byte(0x36); // Komut 0x36: Okuma, CRC Yok, 6 Byte
+  this->write_byte(addr >> 8);
+  this->write_byte(addr & 0xFF);
+  uint64_t val = 0;
+  // App Note Syf 59: Data[0] Lowest Byte, Data[5] Highest Byte'tır
+  for (int i = 0; i < 6; i++) {
+    uint64_t b = this->read_byte();
+    val |= (b << (i * 8));
+  }
+  this->disable();
+  return val;
 }
 
 void PL7211Component::set_gpio_bit(uint8_t bit, bool state) {
+  // Bu fonksiyon artık doğrudan 0-15 arası pin numarasını kabul eder
   if (state) {
-    this->gpio_data_high_ |= (1 << bit);
+    this->gpio_state_ |= (1 << bit);
   } else {
-    this->gpio_data_high_ &= ~(1 << bit);
+    this->gpio_state_ &= ~(1 << bit);
   }
-  // Write to GPIO DATA High byte (0x3905)
-  this->write_register(0x3905, this->gpio_data_high_);
+  // Güvenlik için 16-bit veri adresine (0x3904) blok yazma yapıyoruz
+  this->write_register_16(0x3904, this->gpio_state_);
 }
 
 void PL7211Component::update() {
-  // Read and publish DSP Energy registers (Addresses from App Note Table 6-1)
+  // Voltaj (App Note Syf: 59)
   if (this->voltage_sensor_ != nullptr) {
-    float v = this->read_register_float(0x3078);
-    this->voltage_sensor_->publish_state(v);
+    uint64_t v_raw = this->read_register_48(0x3078);
+    float vrms = (float)v_raw / 16777216.0f; // Formül: Değer / 2^24
+    this->voltage_sensor_->publish_state(vrms);
   }
+  
+  // Akım (App Note Syf: 60)
   if (this->current_sensor_ != nullptr) {
-    float c = this->read_register_float(0x3084);
-    this->current_sensor_->publish_state(c);
+    uint64_t i_raw = this->read_register_48(0x3084);
+    float irms = (float)i_raw / 1073741824.0f; // Formül: Değer / 2^30
+    this->current_sensor_->publish_state(irms);
   }
+  
+  // Aktif Güç (App Note Syf: 61)
   if (this->power_sensor_ != nullptr) {
-    float p = this->read_register_float(0x3090);
-    this->power_sensor_->publish_state(p);
+    uint64_t p_raw = this->read_register_48(0x3090);
+    float power = (float)p_raw / 16777216.0f; // Formül: Değer / 2^24
+    this->power_sensor_->publish_state(power);
   }
-  if (this->power_factor_sensor_ != nullptr) {
-    float pf = this->read_register_float(0x30B4);
-    this->power_factor_sensor_->publish_state(pf);
-  }
+  
+  // Frekans (App Note Syf: 64)
   if (this->frequency_sensor_ != nullptr) {
-    float f = this->read_register_float(0x30C0);
-    this->frequency_sensor_->publish_state(f);
+    uint16_t zcc_cnt = this->read_register_16(0x3018);
+    uint64_t zcc_start = this->read_register_48(0x301E);
+    uint64_t zcc_stop = this->read_register_48(0x3024);
+    uint16_t sample_cnt = this->read_register_16(0x3809);
+    
+    // Sıfıra bölünme hatasını engellemek için güvenlik kontrolü
+    if (sample_cnt > 0 && zcc_stop > zcc_start && zcc_cnt > 1) {
+      float time_diff = (float)(zcc_stop - zcc_start) / (float)sample_cnt;
+      if (time_diff > 0.0f) {
+        float freq = ((zcc_cnt - 1) / 2.0f) / time_diff;
+        this->frequency_sensor_->publish_state(freq);
+      }
+    }
+  }
+
+  // Güç Faktörü (App Note Syf: 62)
+  if (this->power_factor_sensor_ != nullptr) {
+    float vrms = this->voltage_sensor_->state;
+    float irms = this->current_sensor_->state;
+    float power = this->power_sensor_->state;
+    
+    if (vrms > 0.0f && irms > 0.0f) {
+      float pf = power / (vrms * irms);
+      if (pf > 1.0f) pf = 1.0f; // Sınır koruması
+      this->power_factor_sensor_->publish_state(pf);
+    }
   }
 }
 
--- components/pl7211/pl7211.h
+++ components/pl7211/pl7211.h
@@ -4,11 +4,11 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/spi/spi.h"
+#include <cstdint>
 
 namespace esphome {
 namespace pl7211 {
 
-// The PL7211 uses SPI Mode 3 (CPOL=1, CPHA=1) and we operate at 1MHz
 class PL7211Component : public PollingComponent,
                         public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_TRAILING, spi::DATA_RATE_1MHZ> {
  public:
@@ -16,23 +16,22 @@ class PL7211Component : public PollingComponent,
   void update() override;
   void dump_config() override;
 
-  // Sensor Setters
   void set_voltage_sensor(sensor::Sensor *s) { voltage_sensor_ = s; }
   void set_current_sensor(sensor::Sensor *s) { current_sensor_ = s; }
   void set_power_sensor(sensor::Sensor *s) { power_sensor_ = s; }
   void set_power_factor_sensor(sensor::Sensor *s) { power_factor_sensor_ = s; }
   void set_frequency_sensor(sensor::Sensor *s) { frequency_sensor_ = s; }
 
-  // SPI Communication methods
-  void write_register(uint16_t addr, uint8_t data);
-  uint32_t read_register_32(uint16_t addr);
-  float read_register_float(uint16_t addr);
+  // 8-bit, 16-bit ve 48-bit SPI Okuma/Yazma Komutları
+  void write_register_8(uint16_t addr, uint8_t data);
+  void write_register_16(uint16_t addr, uint16_t data);
+  uint16_t read_register_16(uint16_t addr);
+  uint64_t read_register_48(uint16_t addr);
 
-  // GPIO control method
   void set_gpio_bit(uint8_t bit, bool state);
 
  protected:
-  uint8_t gpio_data_high_{0x00}; // Stores the state of GPIO8-GPIO15 (Register 0x3905)
+  uint16_t gpio_state_{0x0000}; // GPIO0-GPIO15 durumlarını tutan 16-bit hafıza
 
   sensor::Sensor *voltage_sensor_{nullptr};
   sensor::Sensor *current_sensor_{nullptr};
@@ -51,7 +50,6 @@ class PL7211Switch : public switch_::Switch, public Component {
     parent_->set_gpio_bit(bit_, state);
     publish_state(state);
   }
-
   PL7211Component *parent_;
   uint8_t bit_;
 };
